5375d9935e6784debbe5c4a2cbe1283aa60945c2
Dapo Momodu <momoduoladapo@gmail.com>
2020-01-24 14:04:39 +0100

    Making Files Open Source

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* SIDE MENU */
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("sideMenu").style.width = "90vw";
+    document.getElementById("sideMenu").style.height = "auto";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("sideMenu").style.width = "0";
+  }
